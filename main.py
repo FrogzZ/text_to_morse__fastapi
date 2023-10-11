451f800b1fb7fc010a9de4e1fb9223cf2d374463
serj <whitewatersergey@gmail.com>
2023-10-11 20:38:18 +0300

    some fix

--- main.py
+++ main.py
@@ -23,17 +23,31 @@ async def api(request: Request):
     return {'1': 1}
 
 
-@app.get("/translate/{lang}/{input_text}")
-async def translate(lang: str, input_text: str):
+@app.get("/translate/{method}/{lang}/{input_text}")
+async def translate(method: str, lang: str, input_text: str):
     lang_list = morse.ru if lang == 'ru' else morse.eu
     res = ''
-    for i in input_text.upper():
-        if i in lang_list:
-            res += lang_list[i]
-            res += ' '
-        elif i == ' ':
-            res += '  '
-    return {'result': res}
+    input_text = input_text[1:]
+    # Text -> Morse
+    if method == 'tm':
+        for i in input_text.upper():
+            if i in lang_list:
+                res += lang_list[i]
+                res += ' '
+            elif i == ' ':
+                res += ' '
+        return {'result': res}
+    # Morse -> Text
+    elif method == 'mt':
+        split_morse = input_text.split(' ')
+        reverse_list = dict(zip(lang_list.values(), lang_list.keys()))
+        for i in split_morse:
+            if i.upper() in reverse_list:
+                res += reverse_list[i.upper()]
+            elif i == '':
+                res += ' '
+        print(reverse_list)
+        return {'result': res}
 
 
 @app.exception_handler(StarletteHTTPException)
@@ -41,7 +55,6 @@ async def my_custom_exception_handler(request: Request, exc: StarletteHTTPExcept
     if exc.status_code == 404:
         return templates.TemplateResponse('404.html', {'request': request})
     else:
-        # Just use FastAPI's built-in handler for other errors
         return await http_exception_handler(request, exc)
 
 
